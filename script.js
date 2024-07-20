00d78391ab5d268e976ef92142fd74e42cba685b
Jean-Emmanuel Gallo <gallojean.emmanuel@gmail.com>
2024-07-20 21:30:28 +0200

    Ajout images / ajout lien facebook / images cliquables

--- script.js
+++ script.js
@@ -111,3 +111,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("imgDisplay");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+document.querySelectorAll('#falbum img').forEach(img => {
+  img.addEventListener('click', function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  });
+});
+
+closeBtn.addEventListener('click', function() {
+  modal.style.display = "none";
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
